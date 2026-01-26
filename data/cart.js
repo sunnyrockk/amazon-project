3d01d0d2fe1e5b919a6f5f0ab303e98a8da46cfc
SuperSimpleDev <sunnypratap859@gmail.com>
2026-01-26 18:13:27 +0530

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,18 @@
 export const cart = [];
 
+export function addToCart(productId) {
+      let matchingItem;
+        cart.forEach((cartitem)=>{
+            if(item.productId === productId){
+              matchingItem = cartitem;
+            }
+             });
+            if(matchingItem){
+                matchingItem.quantity += 1;
+            }else{
+               cart.push ({
+            productId: productId,
+            quantity: 1 
+        });
+         } 
+ }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,51 +1,3 @@
-// const products = [{
-//      image: 'images/products/athletic-cotton-socks-6-pairs.jpg', 
-//      name : 'Black and Gray Gray Athletic Cotton Socks - 6 Pairs',
-//      rating : {
-//         stars: 4.5,
-//         count: 87
-//      },
-     
-//      priceCents: 1090 },{
-//        image: 'images/products/intermediate-composite-basketball.jpg',
-//        name: 'Intermediate Size Basketball',
-//        rating: {
-//         stars: 4,
-//        count:127},
-//        priceCents: 2095
-
-//      },{
-//         image:' images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-//         name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-//         rating:{
-//             stars: 4.5,
-//             count: 56    
-//         },
-//             priceCents: 799,
-//      },
-//      {
-//         image: 'images/products/black-2-slot-toaster.jpg',
-//         name: '2 Slot Toaster - Black',
-//         rating:{
-//             stars: 5,
-//             count: 2197,
-//         },
-//         priceCents: 1899,
-
-//      },
-//      {
-//         image: 'images/products/6-piece-white-dinner-plate-set.jpg',
-//         name: '6 Piece White Dinner Plate Set',
-//         rating:{
-//             stars: 5,
-//             count: 2197,
-//         },
-//         priceCents: 1899,
-
-//      },
-    
-//     ];
-
 import {cart} from '../data/cart.js';
 import { products } from '../data/products.js';
 
@@ -107,33 +59,22 @@ import { products } from '../data/products.js';
 
    document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+ 
+   function updateCartQuantity(){
+        let cartQuantity = 0 ;
+
+                cart.forEach((cartitem) =>{
+                    cartQuantity += cartitem.quantity ;
+                });
+            
+                document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        }
+            
    document.querySelectorAll('.js-add-to-cart')
    .forEach((button) =>{
     button.addEventListener('click',()=>{
         const productId = button.dataset.productId;
-     
-        let matchingItem;
-        cart.forEach((item)=>{
-            if(item.productId === productId){
-              matchingItem = item;
-            }
-             });
-            if(matchingItem){
-                matchingItem.quantity += 1;
-            }else{
-               cart.push ({
-            productId: productId,
-            quantity: 1 
-        });
-         } 
-
-        let cartQuantity = 0 ;
-
-        cart.forEach((item) =>{
-            cartQuantity += item.quantity ;
-        });
-       
-        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-                  
+        addToCart(productId);
+        updateCartQuantity();
     });
    });
